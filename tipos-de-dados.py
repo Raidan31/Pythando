a558768bb4715f98239ba1ba9a71c2a366e8c8f7
Raidan <114839023+Raidan31@users.noreply.github.com>
2022-10-24 01:34:22 -0300

    Aula dos tipos de dados existentes em Python

---
+++ tipos-de-dados.py
@@ -0,0 +1,19 @@
+print("Tipos de Dados")
+
+
+print(1+10) 
+print("Numero Inteiro")
+
+print(5.1+10) 
+print("Numero Flutuante")
+
+print(True)
+print(False)
+print("Boleano")
+
+print("Texto")
+print("String")
+
+
+
+
